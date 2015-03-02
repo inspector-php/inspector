917d80a97c19dc6d6d1a6094abc4f2d0ffceb935
Hongliang <h.wang@linkorb.com>
2015-03-02 17:39:26 +0100

    refactoring

--- src/Formatter/ConsoleFormatter.php
+++ src/Formatter/ConsoleFormatter.php
@@ -15,30 +15,11 @@ class ConsoleFormatter
         foreach ($inspector->getInspections() as $inspection) {
             if ($inspection->hasIssues()) {
                 $o .= " <error>✘ " . $inspection->getShortName() . "</error>\n";
-                /*
-                foreach ($inspection->getIssues() as $issue) {
-                    $o .= "  - <comment>" . $issue->getSubject() . "</comment>\n";
-                    $o .= "       <comment>Details:</comment> " . $issue->getDescription() . "\n";
-                    $o .= "       <comment>Solution:</comment> " . $issue->getSolution() . "\n";
-                    foreach ($issue->getLinks() as $link) {
-                        $o .= "       <comment>Link:</comment> [" . $link->getLabel() . "](" . $link->getUrl() . ")\n";
-                    }
-                }
-                */
                 $o .= $this->getDetails($inspection);
             } else {
                 $o .= " <info>✓ " . $inspection->getShortName() . "</info>\n";
             }
         }
-        
-        /*
-        $o .= "Checks: " . $output->getCheckCount() . " Issues: ";
-        if ($output->getIssueCount()>0) {
-            $o.= "<error>" . $output->getIssueCount() . "</error>";
-        } else {
-            $o.= "<info>" . $output->getIssueCount() . "</info>";
-        }
-        */
         $o .= "\n";
         return $o;
     }
